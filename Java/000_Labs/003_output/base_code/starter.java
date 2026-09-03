a4fa9a1974aff0fe6daa75db57c3af56ec5f8023
skulzz36 <geniusgarduno01@gmail.com>
2026-09-03 22:17:30 +0000

    update

--- Java/000_Labs/003_output/base_code/starter.java
+++ Java/000_Labs/003_output/base_code/starter.java
@@ -9,6 +9,11 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.print("I love black cats."); 
+
+		System.out.print("happy birthday to you.");
+		System.out.print("happy birthday to you.");
+		System.out.print("happy birthday dear whoever");
+		System.out.print("happy birthday to you.");
 	}
 }
--- Java/000_Labs/004_String_class/base_code/starter.java
+++ Java/000_Labs/004_String_class/base_code/starter.java
@@ -9,6 +9,9 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println("Happy birthday to you");
+		System.out.println("Happy birthday to you");
+		System.out.println("happy birthday dear mom");
+		System.out.println("happy birthday to you");
 	}
 }
